a66e69eec6ef5e22d94da4db4505597ba0be2f41
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-02-29 13:09:34 +0530

    Styling post

--- src/features/posts.js
+++ src/features/posts.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './posts.css';
 
 export default function Posts({posts}) {
     return (
         posts.map(post => {
             return (
-                <div key={post.id} className="row">
+                <div key={post.id} className="row post">
                     <div className="col-md-12">
-                        <h2>{post.title}</h2>
-                        <div>
+                        <h2 className="post-header">{post.title}</h2>
+                        <div className="post-body">
                             {post.body}
                         </div>
                     </div>
